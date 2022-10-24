f6841ec43e48c7e55479769221f7b563b477d9be
JimmyRojasE <ji.rojas@duocuc.cl>
2022-10-24 16:19:18 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppJuegos_p2',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
